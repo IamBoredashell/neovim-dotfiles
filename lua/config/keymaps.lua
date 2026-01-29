a99f2ab1a2313a13388d492fcf984bd35f69114f
IamBoredashell <mriganshchaudhary@tutamail.com>
2026-01-29 13:38:29 +0530

    Keycode update added for lsp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,14 @@ keymap("n", "<leader>w", ":w<CR>")
 keymap("n", "<leader>q", ":q<CR>")
 keymap("n", "<leader>h", ":nohlsearch<CR>")
 
+
+--With LSP?
+--Ideally try to make it only execute when lsp is connected (Make function)
+keymap('n', 'gd', vim.lsp.buf.definition, { silent = true })
+keymap("n", "gr", vim.lsp.buf.references, { silent = true })
+keymap("n", "gi", vim.lsp.buf.implementation, { silent = true })
+keymap("n", "<leader>rn", vim.lsp.buf.rename, { silent = true })
+keymap("n", "K", vim.lsp.buf.hover, { silent = true })
+
+
+
