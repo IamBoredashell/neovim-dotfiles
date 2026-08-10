f376eef23a19419324ec830c29b688b8b866a66d
OpinionatedDeer <mriganshchaudhary@tutamail.com>
2026-08-10 22:06:10 +0530

    I added telescope and stuff, right now tree sitter is only thing i do not actualy use.
    Signed-off-by: OpinionatedDeer <mriganshchaudhary@tutamail.com>

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,7 +1,7 @@
 return{
   "lervag/vimtex",
   lazy = false,     -- we don't want to lazy load VimTeX
-  -- tag = "v2.15", -- uncomment to pin to a specific release
+  tag = "v2.18", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
     vim.g.tex_flavor = "latex"
