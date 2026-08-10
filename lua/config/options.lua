2aa81fec60d3550db3257bc425d7d9f67da2e85e
OpinionatedDeer <mriganshchaudhary@tutamail.com>
2026-08-10 22:29:11 +0530

    Added dictionary. Signed-off-by: OpinionatedDeer <mriganshchaudhary@tutamail.com>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,3 +15,23 @@ vim.opt.signcolumn = "yes"
 
 vim.opt.mouse = "a" -- Enable mouse mode
 vim.opt.ruler = false -- Disable the default ruler
+
+
+vim.opt_local.spell = true
+vim.opt_local.spelllang = "en_us"
+--
+-- | Action                                         | Key                           |
+-- | ---------------------------------------------- | ----------------------------- |
+-- | Jump to next misspelling                       | `]s`                          |
+-- | Jump to previous misspelling                   | `[s`                          |
+-- | Show correction suggestions                    | `z=`                          |
+-- | Accept a suggested correction                  | Select it, then Enter         |
+-- | Add word as **correct**                        | `zg`                          |
+-- | Add word as **wrong**                          | `zw`                          |
+-- | Undo “correct” marking                         | `zug`                         |
+-- | Undo “wrong” marking                           | `zuw`                         |
+-- | Mark word as correct **for this session only** | `zG`                          |
+-- | Mark word as wrong **for this session only**   | `zW`                          |
+-- | Spell-check on/off                             | `:set spell` / `:set nospell` |
+-- | Show current spell settings                    | `:set spell?`                 |
+--
